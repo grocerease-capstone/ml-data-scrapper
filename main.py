ad8bf06b31ea6f32be53140fb5593fc398dcf780
Rizal Anggoro <gnoogler4@gmail.com>
2024-11-13 11:37:39 +0700

    chore: fix, add more data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import re
 
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
@@ -12,6 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def scrapper(key: str, categories: str, max_page: int, label: str):
+    print(f"memulai scrapping untuk label: {label}\n")
     template_url: str = "https://www.klikindomaret.com/search/?key={key}&categories={categories}&productbrandid=&sortcol=&pagesize=54&page={page}&startprice=&endprice=&attributes=&ShowItem="
 
     products = []
@@ -61,16 +63,27 @@ def scrapper(key: str, categories: str, max_page: int, label: str):
                 title = wrp_content_element.find('div', class_='title').text
 
                 wrp_price_element = wrp_content_element.find('div', class_='wrp-price')
-                price_element = wrp_price_element.find('div', class_='price')
-                normal_price = price_element.find('span', class_='normal price-value').text
+
+                # cek apakah ada discount atau tidak
+                try:
+                    if wrp_price_element.find('span', class_='strikeout disc-price'):
+                        item_price = wrp_price_element.find('span', class_='discount').text
+                        item_price = re.sub(r"<span>.*?</span>", "", item_price)
+                    else:
+                        price_element = wrp_price_element.find('div', class_='price')
+                        item_price = price_element.find('span', class_='normal price-value').text
+                except:
+                    item_price = 'tidak ada'
+                    print(f'gagal mendapatkan harga untuk item: {title}')
 
                 products.append({
                     'title': title.strip(),
-                    'normal_price': normal_price.strip(),
+                    'normal_price': item_price.strip(),
                     'label': label,
                     'image_url': image_url,
                 })
 
+            print(f"berhasil melakukan percobaan ke-{page_index}")
         except Exception as e:
             print("terjadi error: ", e)
 
@@ -98,10 +111,34 @@ def save_to_csv(data, filename):
 if __name__ == '__main__':
     categories = [
         {
+            'key': 'Kesehatan',
+            'categories': 'kesehatan',
+            'max_page': 6,
+            'label': 'kesehatan',
+        },
+        {
+            'key': 'Segar',
+            'categories': 'ayam',
+            'max_page': 2,
+            'label': 'produk segar',
+        },
+        {
             'key': 'Makanan',
             'categories': 'Makanan',
             'max_page': 18,
             'label': 'makanan',
+        },
+        {
+            'key': 'minuman',
+            'categories': 'beverage',
+            'max_page': 9,
+            'label': 'minuman',
+        },
+        {
+            'key': 'kecantikan',
+            'categories': 'personal-care',
+            'max_page': 4,
+            'label': 'kecantikan',
         },
     ]
 
